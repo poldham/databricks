0d6679603700f072e99531038f5782a2096d0fda
poldham <poldham@mac.com>
2019-09-20 13:36:32 +0000

    correction to table name. create species_doi_results

--- source_mag_tables.R
+++ source_mag_tables.R
@@ -11,12 +11,49 @@ sc <- spark_connect(method = "databricks")
 
 # source tables
 # may need to arrange field names when loading
-affiliations <- tbl(sc, "affiliations_grid_spark_tbl")
-affiliations_papers <- tbl(sc, "affiliations_papers_spark_tbl")
+affiliation <- tbl(sc, "affiliations_grid_spark_tbl")
+affiliation_papers <- tbl(sc, "affiliation_papers_spark_tbl")
 papers <- tbl(sc, "papers_spark_tbl")
 fos <- tbl(sc, "fos_spark_tbl") # check col format
 fos_papers <- tbl(sc, "fos_papers_spark_tbl") # check col format
 authors <- tbl(sc, "authors_spark_tbl")
+
+# source the species dois table
+
+mag_species_doi <- tbl(sc, "mag_species_doi")
+
+# join the matches from the papers table onto the species table
+
+species_doi_results <- inner_join(mag_species_doi, papers, by = "doi")
+
+# write the table to the dbfs
+
+spark_write_table(species_doi_results, "species_doi_results")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # set the country
 
