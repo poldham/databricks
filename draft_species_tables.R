82f96a730191ed7c4a576cb901bb64769dcb7f59
poldham <poldham@mac.com>
2019-09-24 08:25:21 +0000

    table joins for draft landscape

---
+++ draft_species_tables.R
@@ -0,0 +1,6 @@
+# draft tables for species landscape in scientific literature
+# use the rough ids table to join paperid to affiliation and other tables
+
+# source the rough id table
+
+species_found_id <- tbl(sc, "species_found_rough_id_spark")
